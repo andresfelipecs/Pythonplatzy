040ce9022e861642d69db4bab2421b7e2957892b
felipe <fepipe22@gmail.com>
2022-08-23 14:37:27 +0200

    hangman game lists done weneed to replace the letters in the new list with the lines

--- hangman/hangmangame.py
+++ hangman/hangmangame.py
@@ -3,12 +3,18 @@ import random
 
 def game(words):
     word_selection = random.choice(words)
-    letters = {letter for letter in word_selection if letter != '\n'}
-    list = []
-    for number in enumerate(letters):
-        pass
-     
+    letters = [letter for letter in word_selection if letter != '\n']
+    single_letter = input(str('Please enter the letter to check: '))
+    list = ['_' for i in enumerate(letters)]
+    #print(list)
     
+    if single_letter in letters:
+        list2 = [single_letter]
+        new_list = [element for element in letters if element in list2]
+        print(list)
+    else:
+        print(list)
+
     print(word_selection)
     print(letters)
     
