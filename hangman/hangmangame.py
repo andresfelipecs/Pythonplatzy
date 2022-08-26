c8bf842b4a3e1fe496084498c08b0099714178b7
felipe <fepipe22@gmail.com>
2022-08-26 11:42:16 +0200

    fixed in order to keep the same value in every rep from loop

--- hangman/hangmangame.py
+++ hangman/hangmangame.py
@@ -1,35 +1,35 @@
 
-from hashlib import new
 import random
 
 def game(words):
     word_selection = random.choice(words)
-    word_selection = "mongo"
+    word_selection = "holaamigo"
     # create a lit comp for letter in word
     letters = [letter for letter in word_selection if letter != '\n']
 
 
     # ask the user to input a letter they want to guess from word
-    single_letter = input(str('Please enter the letter to check: '))
-    new_list = letters[:]
+    list = letters[:]
+    for i, n in enumerate(list):
+        list[i] = "_"
     
-
+    for i in range(3):        
     # replace letters with '_'
-    new_list = ['_' for i, _ in enumerate(new_list)]
-    # for i, _ in enumerate(new_list):
-    #     new_list[i] = "_"
-        
     
+            single_letter = input(str('Please enter the letter to check: '))
+        
     #compare single letter with all letters to match
-    #new_list[i] = [ i for i, _ in enumerate(letters) if single_letter == i]
-    for index_val, elem in enumerate(letters):
-        if single_letter == elem:
-            new_list[index_val] = elem
+            for index_val, elem in enumerate(letters):
+                if single_letter == elem:
+                    list[index_val] = elem
+                    
+                    
+            #single_letter = input(str('Please enter the letter to check: '))
+            print(list)
 
 
-    print(word_selection)
-    print(letters)
-    print(new_list)
+    # print(word_selection)
+    # print(letters)
     
 
 
